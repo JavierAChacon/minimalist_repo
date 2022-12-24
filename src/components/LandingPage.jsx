39a01aaec7f53af65df1e38076e4a0399d084e5c
JavierChacon <alejandro.chr@outlook.es>
2022-12-24 12:38:19 -0400

    developing the navbar

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/landingPage.css'
+
+
+const LandingPage = () => {
+  return(
+  <div className="landingPage">
+   <span>I'm Javier Chacon a Front-End Developer</span>
+  </div>
+  )
+}
+
+export default LandingPage
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import '../Styles/navbar.css'
+import { useState } from 'react'
+import { Twirl as Hamburger } from 'hamburger-react'
+
+
+
+const Navbar = () => {
+  
+  const [isOpen, setIsOpen] = useState(false)
+  
+  
+  return (
+    <header className='navbar'>
+      <Hamburger rounded size={24} duration={0.45} direction="right"  distance="lg"/>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Projects</a></li>
+        <li><a href="">Technologies</a></li>
+        <button><a href="">Contact</a></button>
+      </ul>
+    </header>
+  )
+}
+
+export default Navbar
