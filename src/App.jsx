992970748e6a5bf6965f36140f6640499578b59a
JavierChacon <alejandro.chr@outlook.es>
2022-12-27 03:34:00 -0400

    added the landing page and responsive mobile

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import './Styles/App.css'
 import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage'
 
+
 function App() {
   return (
     <div className="App">
      <Navbar/>
-     {/* <LandingPage/> */}
+     <LandingPage/>
     </div>
   )
 }
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 import '../Styles/landingPage.css'
+import Photo from '../assets/images/saly-10.png'
+
 
 
 const LandingPage = () => {
   return(
   <div className="landingPage">
-   <span>I'm Javier Chacon a Front-End Developer</span>
+
+    <div className='tittle'>
+
+      <div className='text'>
+        <h1>I'm Javier Chacon</h1>
+        <h2>a Front-End Developer</h2>
+      </div>
+
+      <div className='shape'>
+        <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+        <path fill="#FF0066" d="M68.8,-23C76.6,1.7,61.7,33,37,51C12.2,69,-22.3,73.6,-43.4,58.6C-64.6,43.5,-72.3,8.7,-62.8,-18.3C-53.4,-45.3,-26.7,-64.6,1.9,-65.2C30.5,-65.8,61,-47.8,68.8,-23Z" transform="translate(100 100)" />
+        </svg>
+      </div>
+
+    </div>
+
+      <div className='image'>
+        <img src={Photo}/>
+      </div>
   </div>
   )
 }
