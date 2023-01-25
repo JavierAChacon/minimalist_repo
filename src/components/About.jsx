a1fd2eb11c107b28437b504f71582a719b4747fc
JavierChacon <alejandro.chr@outlook.es>
2023-01-25 12:01:11 -0400

    new cv link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
     <div className="about" id="about">
       <div data-aos="flip-left" className="description">
         <p>I am a proactive person, who is always on the cutting edge of software development. Currently, I am available for new challenges and experiences, with the goal of increasing my value as a computer science engineer.</p>
-        <a href="https://drive.google.com/file/d/14djBhNa5nya06xdgXz1rhcFvIlE8RTTS/view?usp=sharing" target="_blank">See C.V</a>
+        <a href="https://drive.google.com/file/d/19vP2wRFi0qUsdKfRszz5WkEFz9wp5eOP/view?usp=sharing" target="_blank">See C.V</a>
       </div>
       <div data-aos="fade-right" className="aboutPhoto">
         <img src={AboutPhoto} alt="" />
